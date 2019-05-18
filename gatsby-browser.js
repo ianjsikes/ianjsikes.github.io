8d53843fa7d08072c6bb05cb2dd15eac760aa3b4
Ian J Sikes <ianjsikes@gmail.com>
2019-05-18 15:10:25 -0700

    Updates

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-// Custom typefaces
-import 'typeface-fira-mono'
-import 'typeface-open-sans'
-import './src/styles/global.css'
---
+++ styles-c855a7e03619cce213b5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{107:function(n,o,w){},108:function(n,o,w){},109:function(n,o,w){}}]);
+//# sourceMappingURL=styles-c855a7e03619cce213b5.js.map
